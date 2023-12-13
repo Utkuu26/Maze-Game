a42ffd1c1d56cfc1f11b9dae2e730ca64cc73c20
Utku Gezensoy <utkugezensoy@gmail.com>
2023-12-14 02:52:11 +0300

    Character's Death Animation and Post-Death Processes Are Completed.
    - When the character dies, the death animation kicks in.
    - The character is teleported to the beginning of the game.
    - The game-over screen appears.

--- Maze Game/Assets/Scripts/PlayerHealth.cs
+++ Maze Game/Assets/Scripts/PlayerHealth.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int currentHealth = 100;   
-    public bool isTakeDamage; 
+    public int currentHealth = 100;  
+    private bool isDead = false;
+    public GameObject gameOverArea;
+
 
     private void Start()
     {
-    
+        gameOverArea.SetActive(false);
     }
 
     public void TakeDamage(int damageAmount)
     {
-        currentHealth -= damageAmount;  
-
-        if (currentHealth <= 0)
+        if (!isDead)
         {
-            Die(); 
+            currentHealth -= damageAmount;
+
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
         }
     }
 
     void Die()
     {
-        // Buraya karakterin ölmeyle ilgili işlemleri ekleyebilirsiniz
-        Debug.Log("Game Over");
+         if (!isDead)
+        {
+            isDead = true;
+            Debug.Log("Game Over");
+            GetComponent<Animator>().SetTrigger("Die");
+
+            ShowGameOverScreen();
+            StartCoroutine(RestartScene());
+        }
     }
+
+    void ShowGameOverScreen()
+    {
+        gameOverArea.SetActive(true);
+    }
+
+    IEnumerator RestartScene()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
